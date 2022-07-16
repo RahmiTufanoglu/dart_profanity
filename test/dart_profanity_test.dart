1fbb33749f9f6e55f01691e961d7491beaf92c06
Rahmi Tufanoglu <rahmi.tufanoglu@gmail.com>
2022-07-16 16:56:42 +0200

    add first test

--- test/dart_profanity_test.dart
+++ test/dart_profanity_test.dart
@@ -1,15 +1,17 @@
+import 'package:dart_profanity/src/dart_profanity_base.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
-    //final dartProfanity = DartProfanity();
+  group('de language tests', () {
+    final dartProfanity = DartProfanity(languageCode: 'de');
 
     setUp(() {
       // Additional setup goes here.
     });
 
-    test('First Test', () {
-      //expect(awesome.isAwesome, isTrue);
+    test('Contains vulgar word', () {
+      final result = dartProfanity.containsProfanity('Arschloch');
+      expect(result, isTrue);
     });
   });
 }
