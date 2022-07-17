624694357939097d1d20096bdd4471ff0ed8a13b
Rahmi Tufanoglu <rahmi.tufanoglu@gmail.com>
2022-07-17 21:02:45 +0200

    fix: tests

--- test/dart_profanity_test.dart
+++ test/dart_profanity_test.dart
@@ -3,37 +3,56 @@ import 'package:test/test.dart';
 
 void main() {
   group('de language tests', () {
-    final dartProfanity = DartProfanity(languageCode: 'de');
+    final dartProfanity = DartProfanity();
+    final dartProfanityDe = DartProfanity(languageCode: 'de');
 
     setUp(() {
       // Additional setup goes here.
     });
 
     test('Contains vulgar word', () {
-      final result = dartProfanity.containsProfanity('Arschloch');
-      expect(result, isTrue);
+      final result1 = dartProfanity.containsProfanity('DU bist ein AsS!');
+      expect(result1, isTrue);
     });
 
-    test('Censore vulgar word', () {
-      final result = dartProfanity.censor(characters: 'Arschloch');
-      expect(result, 'A********');
+    test('Contains vulgar word de', () {
+      final result1 = dartProfanityDe.containsProfanity('Arschloch');
+      expect(result1, isTrue);
+
+      final result2 = dartProfanityDe.containsProfanity('DuWichser.');
+      expect(result2, isTrue);
+
+      final result3 = dartProfanityDe.containsProfanity('Du bist ein mega Arschloch & DuWichser.');
+      expect(result3, isTrue);
     });
 
-    test('Censor vulgar word with random characters', () {
+    test('Censore vulgar word de', () {
+      final result = dartProfanityDe.censor(characters: 'Arschloch');
+      expect(result, '*********');
+    });
+
+    test('Censor vulgar word with asterix', () {
       final result = dartProfanity.censor(
-        characters: 'Arschloch',
-        bleep: CensorBleepType.asterix,
+        characters: 'dU bist ein badAsS!',
+        bleepType: CensorBleepType.asterix,
       );
-      expect(result[0], 'A');
+      expect(result, 'dU bist ein bad***!');
+    });
+
+    test('Censor vulgar word with random characters de', () {
+      final result = dartProfanityDe.censor(
+        characters: 'Arschloch',
+        bleepType: CensorBleepType.asterix,
+      );
       expect(result.substring(1, result.length), '********');
     });
 
-    test('Censor vulgar word with random characters', () {
-      final result = dartProfanity.censor(
+    test('Censor vulgar word with random characters de', () {
+      final result = dartProfanityDe.censor(
         characters: 'Arschloch',
-        bleep: CensorBleepType.characters,
+        bleepType: CensorBleepType.characters,
       );
-      expect(result[0], 'A');
+      expect(result, isNot('*********'));
     });
   });
 }
