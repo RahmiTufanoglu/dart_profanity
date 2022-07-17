02914224e5930aa36e805d5856a5154a61630c3e
Rahmi Tufanoglu <rahmi.tufanoglu@gmail.com>
2022-07-17 21:14:36 +0200

    fix: minor improvements

--- example/dart_profanity_example.dart
+++ example/dart_profanity_example.dart
@@ -1,24 +1,28 @@
 import 'package:dart_profanity/dart_profanity.dart';
 
 void main() {
-  //final dartProfanity = DartProfanity(languageCode: 'de');
-  final dartProfanity = DartProfanity();
+  final dartProfanity = DartProfanity(languageCode: 'de');
   final censored1 = dartProfanity.censor(characters: 'Du bist ein mega Arschloch & DuWichser.');
-  final censored2 = dartProfanity.censor(
-    characters: 'Du bist ein mega Arschloch & DuWichser.',
-    bleep: CensorBleepType.characters,
-  );
-  final censored3 = dartProfanity.censor(
-    characters: 'Leck mich am Arsch.',
-    bleep: CensorBleepType.characters,
-  );
-  final censored4 = dartProfanity.censor(
-    characters: 'Leck mich am Ass.',
-    bleep: CensorBleepType.characters,
-  );
-
   print('censored1: $censored1');
+  final dartProfanity2 = DartProfanity();
+  final censored2 = dartProfanity2.censor(characters: 'Du bist ein mega arschloch & DuAsS!');
   print('censored2: $censored2');
-  print('censored2: $censored3');
-  print('censored2: $censored4');
+  final censored3 = dartProfanity2.censor(
+    characters: 'dU bist ein badAsS!',
+    bleepType: CensorBleepType.asterix,
+  );
+  print('censored3: $censored3');
+  final censored4 = dartProfanity.censor(
+    characters: 'Du bist ein mega Arschloch & DuWichser.',
+    bleepType: CensorBleepType.characters,
+  );
+  print('censored4: $censored4');
+  //final censored3 = dartProfanity.censor(
+  //  characters: 'Leck mich am Arsch.',
+  //  bleep: CensorBleepType.characters,
+  //);
+  //final censored4 = dartProfanity.censor(
+  //  characters: 'Leck mich am Ass.',
+  //  bleep: CensorBleepType.characters,
+  //);
 }
--- lib/src/dart_profanity_base.dart
+++ lib/src/dart_profanity_base.dart
@@ -38,9 +38,6 @@ class DartProfanity {
     CensorBleepType bleepType = CensorBleepType.asterix,
     CensorType censorType = CensorType.full,
   }) {
-    //final word = characters.toLowerCase().split(' ');
-    //print('word $word');
-
     return characters
         .split(' ')
         .asMap()
@@ -48,12 +45,12 @@ class DartProfanity {
         .map((e) {
           final list = profanityList.where((element) {
             return e.value.toLowerCase().contains(element);
-          }).toList();
+          }).toList()
+            ..sort((a, b) => b.length.compareTo(a.length));
 
-          final strList = list..sort((a, b) => b.length.compareTo(a.length));
           String? longestProfanity;
-          if (strList.isNotEmpty) {
-            longestProfanity = strList.first;
+          if (list.isNotEmpty) {
+            longestProfanity = list.first;
           }
 
           if (longestProfanity != null) {
@@ -61,108 +58,13 @@ class DartProfanity {
               bleep: bleepType,
               length: longestProfanity.length - censorType.lengthDisplayed,
             );
-
-            final result = e.value.toLowerCase().replaceAll(longestProfanity, censored);
-            return result;
+            return e.value.toLowerCase().replaceAll(longestProfanity, censored);
           }
 
           return e.value;
         })
         .toList()
         .join(' ');
-
-    //final foo = profanityList.where((element) {
-    //  return word
-    //      .where((el) {
-    //        //final list = profanityList.where((element) => word.contains(element)).toList();
-    //        //final longestProfanity = list.reduce((a, b) => a.length > b.length ? a : b);
-    //        print(el);
-    //        return el.contains(element);
-    //      })
-    //      .toList()
-    //      .isNotEmpty;
-    //}).toList();
-//
-    //print('foo $foo');
-
-    return '';
-    //return characters
-    //    .split(' ')
-    //    .asMap()
-    //    .entries
-    //    .map((e) {
-    //      final word = e.value.toLowerCase();
-    //      print('word: $word');
-    //      final splitted = word.split(' ');
-    //      print('===============|A|===============> $splitted');
-    //      final test = splitted.indexWhere((element) => profanityList.contains(element));
-    //      print('test $test');
-//
-    //      // final a = e.value.split('').indexWhere((element) => longestProfanity.contains(element));
-    //      //       final word = e.value.toLowerCase();
-    //      //       final list = profanityList.where((element) => word.contains(element)).toList();
-    //      //       if (list.isNotEmpty) {
-    //      //         final longestProfanity = list.reduce((a, b) => a.length > b.length ? a : b);
-    //      //         final notFirstLetter = longestProfanity.substring(1, longestProfanity.length);
-    //      //         final censored = CensorBleepType.censored(
-    //      //           bleep: bleep,
-    //      //           length: longestProfanity.length - 1,
-    //      //         );
-////
-    //      //         print('===============|B|===============> ${longestProfanity}');
-    //      //         final res = word.split(longestProfanity);
-////
-    //      //         final empty = res.first.isEmpty;
-    //      //         print('empty $empty');
-////
-    //      //         final index = empty ? 0 : 1;
-////
-    //      //         print('index $index');
-    //      //         print('===============|C|===============> ${res}');
-////
-    //      //         return word.substring(index, index + 1);
-//
-    //      // //badAss23232 split 2 => pos 1
-    //      // //badsdasd221Ass split 1 =>
-////
-    //      // print('===============|A|===============> ${word}');
-    //      // final b = word.split(longestProfanity);
-    //      // print('===============|C|===============> ${b}');
-////
-    //      // // badAss! [bad, !] = 2   =>
-    //      // // bad!Ass [bad!] = 1     =>
-////
-    //      // final test = word.split(b.first);
-    //      // print(test.last);
-////
-    //      // //print(e.value);
-    //      // //print(notFirstLetter);
-    //      // ///
-    //      // final index = b.first.isEmpty ? 0 : (word.length - test.length) - 2;
-    //      // print('index: $index');
-    //      // final result = word.replaceAll(notFirstLetter, censored);
-    //      // //return result;
-    //      // //final firstLetter = result[0].toUpperCase();
-    //      // //print('===============|X|===============> ${e.value}');
-    //      // //print('===============|A|===============> ${word.substring(index, word.length)}');
-    //      // //final foo = word.substring(index, word.length);
-    //      // //final firstLetter = e.value.split('')[index];
-    //      // final firstLetter = e.value.split('')[index];
-    //      // print('firstLetter $firstLetter');
-    //      // //final xx = result.split('')[index] = firstLetter;
-    //      // //print('xx $xx');
-////
-    //      // return result.replaceRange(index, index + 1, firstLetter);
-    //      // //print('|===============> ${boo}');
-    //      // //print('result|===============> ${result}');
-    //      // //final b = e.value.split('').indexWhere((element) => foo.contains(element));
-    //      // //print('===============|B|===============> ${b}');
-    //      // //return result.substring(index - 1, word.length);
-    //      //}
-    //      return e.value;
-    //    })
-    //    .toList()
-    //    .join(' ');
   }
 }
 
