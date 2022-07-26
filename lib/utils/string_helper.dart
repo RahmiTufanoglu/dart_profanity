d023cbac7dceb48808efdf12b2963df78657fd71
Rahmi Tufanoglu <rahmi.tufanoglu@gmail.com>
2022-07-26 21:57:09 +0200

    chore: add string_helper.dart documentation

--- lib/utils/string_helper.dart
+++ lib/utils/string_helper.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 class StringHelper {
   StringHelper._();
 
+  /// Outputs random characters.
   static String getRandomCharacters(int length) {
     final random = Random.secure();
     final values = List<int>.generate(length, (i) => random.nextInt(33) + 89);
