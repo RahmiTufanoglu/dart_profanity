ddf946c03ff5d6bbf67f5b7e3007ca66b7c1276d
Rahmi Tufanoglu <rahmi.tufanoglu@gmail.com>
2022-08-07 20:07:26 +0200

    refactor: improve CensorBleepType

--- lib/src/censor_bleep_type.dart
+++ lib/src/censor_bleep_type.dart
@@ -1,22 +1,41 @@
+import 'package:dart_profanity/dart_profanity.dart';
 import 'package:dart_profanity/utils/string_helper.dart';
 
 /// Creates an [CensorBleepType].
 /// CensorBleepType asterix censors the whole String with asterix symbols.
 /// CensorBleepType characters censor the whole String with random characters.
 enum CensorBleepType {
-  asterix,
-  characters;
+  asterix(CensorType.firstLetter),
+  characters(CensorType.firstLetter);
+
+  const CensorBleepType(this.censorType);
+
+  final CensorType censorType;
 
   static String censored({
+    required String word,
+    required String profanity,
     required dynamic bleep,
-    required int length,
+    required CensorType censorType,
   }) {
-    if (bleep is String) return bleep.toString() * length;
-    if (bleep is CensorBleepType) {
-      if (bleep == CensorBleepType.asterix) return '*' * length;
-      if (bleep == CensorBleepType.characters) return StringHelper.getRandomCharacters(length);
+    late final String censoredWord;
+    final profanityLength = profanity.length;
+    final firstLetterNotCensored = censorType.notCensoredLength == 1 ? profanity[0] : '';
+    final censoredProfanityLength = profanityLength - (censorType.notCensoredLength == 1 ? 1 : 0);
+    if (bleep is String) {
+      //censoredWord = firstLetterNotCensored + bleep.toString() * profanityLength;
+      censoredWord = firstLetterNotCensored + bleep.toString() * censoredProfanityLength;
+    } else if (bleep is CensorBleepType) {
+      if (bleep == CensorBleepType.asterix) {
+        censoredWord = firstLetterNotCensored + '*' * censoredProfanityLength;
+      } else if (bleep == CensorBleepType.characters) {
+        censoredWord = firstLetterNotCensored + StringHelper.getRandomCharacters(censoredProfanityLength);
+      }
+    } else {
+      censoredWord = firstLetterNotCensored + '*' * censoredProfanityLength;
     }
-    return '*' * length;
+    final result = word.replaceAll(profanity, censoredWord);
+    return result;
   }
 
   @override
