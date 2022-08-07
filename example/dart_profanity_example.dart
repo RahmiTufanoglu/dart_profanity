f2ffef1dcea9fc28166da27f781e682914b00947
Rahmi Tufanoglu <rahmi.tufanoglu@gmail.com>
2022-08-07 20:06:49 +0200

    refactor: update example

--- example/dart_profanity_example.dart
+++ example/dart_profanity_example.dart
@@ -1,20 +1,28 @@
 import 'package:dart_profanity/dart_profanity.dart';
 
 void main() {
-  final profanity = Profanity(languages: ['de', 'en', 'gg']);
-  final censored = profanity.censor('Du bist ein mega Arschloch & DuWichser.');
-  print('censored0: $censored');
-
-  final censored1 = profanity.censor('Du bist ein mega Arschloch & DuWichser.');
-  print('censored1: $censored1');
-
-  final dartProfanity2 = Profanity();
-  final censored2 = dartProfanity2.censor('Du bist ein mega arschloch & DuAsS!');
-  print('censored2: $censored2');
-
-  final censored3 = dartProfanity2.censor(
-    'Du bist ein mega arschloch & DuAsS!',
+  final profanity = Profanity();
+  final result = profanity.censor(
+    'FuckYou assfuckerAsshole and pUSSy!',
+    bleepType: CensorBleepType.asterix,
     censorType: CensorType.firstLetter,
   );
-  print('censored3: $censored3');
+  print('result: $result');
+
+  //final profanity = Profanity(languages: ['de', 'en', 'gg']);
+  //final censored = profanity.censor('Du bist ein mega Arschloch & DuWichser.');
+  //print('censored0: $censored');
+
+  //final censored1 = profanity.censor('Du bist ein mega Arschloch & DuWichser.');
+  //print('censored1: $censored1');
+
+  //final dartProfanity2 = Profanity();
+  //final censored2 = dartProfanity2.censor('Du bist ein mega arschloch & DuAsS!');
+  //print('censored2: $censored2');
+
+  //final censored3 = dartProfanity2.censor(
+  //  'Du bist ein mega arschloch & DuAsS!',
+  //  censorType: CensorType.firstLetter,
+  //);
+  //print('censored3: $censored3');
 }
